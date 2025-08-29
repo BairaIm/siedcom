68cc0843297e63e464c93905cad0af0b01131c50
unknown <imeevaba@mail.ru>
2025-08-29 23:54:48 +0300

    delete comment in head (index)

--- php/script.php
+++ php/script.php
@@ -2,10 +2,11 @@
     // Получаем данные из полей ввода
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $tel = $_POST['tel'];
     $comment = $_POST['comment'];
     $to = 'example@admin.com';
     $header = 'Комментарий от пользователя';
     // Собираем все данные в письмо
-    $message = "Имя пользователя: $name \nЭлектронная почта: $email \nКомментарий: $comment";
+    $message = "Имя пользователя: $name \nЭлектронная почта: $email \nТелефон: $tel \nКомментарий: $comment";
     // Отправляем письмо
     $send = mail($to, $header, $message, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
